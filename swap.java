9b3ff95d9900605a91b2566ac3d40c2be214f49e
Git-Charan717 <saicharangolkonda34@gmail.com>
2025-11-06 15:20:58 +0530

    first commit

---
+++ swap.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+public class swap{
+public static void main(String args[]){
+int temp, a=2;
+int b=7;
+system.out.println("Before Swapping number ="+a+"+b);
+int temp=a;
+int a=b;
+int a=temp;
+system.out.println("After Swapping number= "+a+"+b);
+}
+}
