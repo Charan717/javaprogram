13f47d573e19f73547693f3bcff866eb7f238f52
Charan717 <saicharangolkonda34@gmail.com>
2025-11-20 15:10:10 +0530

    Update swap.java

--- swap.java
+++ swap.java
@@ -1,12 +1,13 @@
-import java.lang.*;
-public class swap{
-public static void main(String args[]){
+import java.io.*;
+class swap {
+public static void main(String[] args) {
+int a = 2;
+int b = 7;
 int temp;
-int a=2;
-int b=7;
-System.out.println("Before swapping a="+a+"b="+b);
-temp=a;
-a=b;
-b=temp;
-System.out.println("After swapping a="+a+ "b="+b);
-}}
+temp = a;
+a = b;
+b = temp;
+System.out.println("After swapping, a = " + a + " and b = " + b);
+    }
+
+}
